28bbf86ad638e5bf8536427487a923df92cb4fdd
Thoma <Thoma@LAPTOP-P1UTSHS3>
2024-01-16 21:13:07 +0100

    Aggiungere i file di progetto.

---
+++ Formula1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Formula1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridViewPiloti = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridViewTeam = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridViewGareVinte = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.dataGridViewPilotiTeam = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPiloti)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTeam)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGareVinte)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPilotiTeam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1227, 724);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.dataGridViewPiloti);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1219, 695);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // dataGridViewPiloti
+            // 
+            this.dataGridViewPiloti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPiloti.Location = new System.Drawing.Point(33, 171);
+            this.dataGridViewPiloti.Name = "dataGridViewPiloti";
+            this.dataGridViewPiloti.RowHeadersWidth = 51;
+            this.dataGridViewPiloti.RowTemplate.Height = 24;
+            this.dataGridViewPiloti.Size = new System.Drawing.Size(1086, 467);
+            this.dataGridViewPiloti.TabIndex = 1;
+            this.dataGridViewPiloti.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPiloti_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridViewTeam);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1219, 695);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewTeam
+            // 
+            this.dataGridViewTeam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTeam.Location = new System.Drawing.Point(127, 142);
+            this.dataGridViewTeam.Name = "dataGridViewTeam";
+            this.dataGridViewTeam.RowHeadersWidth = 51;
+            this.dataGridViewTeam.RowTemplate.Height = 24;
+            this.dataGridViewTeam.Size = new System.Drawing.Size(944, 502);
+            this.dataGridViewTeam.TabIndex = 2;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridViewGareVinte);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1219, 695);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewGareVinte
+            // 
+            this.dataGridViewGareVinte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGareVinte.Location = new System.Drawing.Point(100, 187);
+            this.dataGridViewGareVinte.Name = "dataGridViewGareVinte";
+            this.dataGridViewGareVinte.RowHeadersWidth = 51;
+            this.dataGridViewGareVinte.RowTemplate.Height = 24;
+            this.dataGridViewGareVinte.Size = new System.Drawing.Size(904, 413);
+            this.dataGridViewGareVinte.TabIndex = 3;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridViewPilotiTeam);
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1219, 695);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            this.tabPage4.Click += new System.EventHandler(this.tabPage4_Click);
+            // 
+            // dataGridViewPilotiTeam
+            // 
+            this.dataGridViewPilotiTeam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPilotiTeam.Location = new System.Drawing.Point(93, 165);
+            this.dataGridViewPilotiTeam.Name = "dataGridViewPilotiTeam";
+            this.dataGridViewPilotiTeam.RowHeadersWidth = 51;
+            this.dataGridViewPilotiTeam.RowTemplate.Height = 24;
+            this.dataGridViewPilotiTeam.Size = new System.Drawing.Size(954, 424);
+            this.dataGridViewPilotiTeam.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 847);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPiloti)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTeam)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGareVinte)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPilotiTeam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridViewPiloti;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridViewTeam;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridViewGareVinte;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView dataGridViewPilotiTeam;
+        private System.Windows.Forms.TextBox txtFiltroNome;
+        private System.Windows.Forms.Button btnFiltraPiloti;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Formula1/Form1.cs
@@ -0,0 +1,191 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Formula1
+{
+    public partial class Form1 : Form
+    {
+        private const string ConnectionString = "server=127.0.0.1;uid=programma;pwd=12345;database=formula_one_db";
+        public Form1()
+        {
+            InitializeComponent();
+            tabControl1.SelectedIndexChanged += TabControl1_SelectedIndexChanged;
+
+        }
+
+        private void TabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Quando la scheda selezionata cambia, carica i dati corrispondenti
+            switch (tabControl1.SelectedIndex)
+            {
+                case 0:
+                    LoadPilotiData();
+                    break;
+                case 1:
+                    LoadTeamData();
+                    break;
+                case 2:
+                    LoadGareVinteData();
+                    break;
+                case 3:
+                    LoadPilotiTeam();
+                    break;
+                    // Aggiungi altri casi per eventuali ulteriori schede
+            }
+        }
+
+        private void LoadPilotiData(string filtroNome = "")
+        {
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM piloti";
+
+                if (!string.IsNullOrEmpty(filtroNome))
+                {
+                    // Aggiungi il filtro per il nome
+                    sql += $" WHERE cognome LIKE '%{filtroNome}%'";
+                }
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd;
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridViewPiloti.DataSource = dati;
+
+                conn.Close();
+            }
+        }
+
+
+        private void LoadTeamData()
+        {
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM team;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd;
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridViewTeam.DataSource = dati;
+
+                conn.Close();
+            }
+        }
+
+        private void LoadGareVinteData()
+        {
+            using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM gare_vinte;";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = cmd;
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+                dataGridViewGareVinte.DataSource = dati;
+
+                conn.Close();
+            }
+        }
+
+        private void LoadPilotiTeam()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(ConnectionString))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT piloti.cognome, team.nome_team, piloti_team.anno_ingresso " +
+              "FROM piloti_team " +
+              "JOIN piloti ON piloti_team.pilota_id = piloti.pilota_id " +
+              "JOIN team ON piloti_team.team_id = team.team_id;";
+
+
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                    MyAdapter.SelectCommand = cmd;
+                    DataTable dati = new DataTable();
+                    MyAdapter.Fill(dati);
+                    dataGridViewPilotiTeam.DataSource = dati;
+
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore durante il caricamento dei dati da piloti_team: {ex.Message}", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridViewPiloti.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewTeam.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewGareVinte.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewPilotiTeam.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            tabControl1.SelectedIndex = 0;
+
+
+            
+
+        }
+
+        private void dataGridViewPiloti_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFiltraPiloti_Click(object sender, EventArgs e)
+        {
+            LoadPilotiData(txtFiltroNome.Text);
+
+        }
+
+        private void txtFiltroNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadPilotiData(textBox1.Text);
+        }
+    }
+}
+    
+
+
+
